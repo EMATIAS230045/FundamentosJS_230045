fd95515982cf2ee614ae3d5743b3a5fc2972cd83
Erick Matias Granillo Mejia <230045@utxicotepec.edu.mx>
2024-09-17 15:22:29 -0600

    practica 4 Ejercio 03
    Agregado de los datos String, Init y Symbol

---
+++ JS/Ejercio03.js
@@ -0,0 +1,39 @@
+// string o cadenas de texto
+
+//const alumno = "Juan"
+//let producto1 = 'Monitor 20 pulgadas'
+//const numero = "30"
+//const numero2 = 30
+
+//console.log(typeof numero)
+//console.log(typeof numero2)
+
+//BigInit
+
+//const numerogrande = BigInt(918298193891887139831091038173813)
+//console.log(typeof numerogrande)
+
+// const numero = 10
+// const numero2 = 20
+
+// console.log( numero + Number(numerogrande))
+
+// const numero = "30"
+// const numero2 = 30
+
+// console.log(typeof String(numero2))
+// console.log(typeof Number(numero))
+
+//symbol
+
+// const primerSymbol = Symbol(30)
+// const segundoSymbol = Symbol(30)
+
+// console.log( primerSymbol == segundoSymbol )
+// console.log(primerSymbol.valueOf() )
+// console.log(segundoSymbol.valueOf() )
+
+//null
+
+const descuento = null
+console.log(typeof descuento)
