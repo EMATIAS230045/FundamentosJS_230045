4df3841fa4dc0d0cf226e8506da9bfdf55e0351e
Erick Matias Granillo Mejia <230045@utxicotepec.edu.mx>
2024-09-24 16:28:52 -0600

    Practica 5 (1)
    guardado de practica

--- JS/Ejercio03.js
+++ JS/Ejercio03.js
@@ -1,6 +1,5 @@
 // string o cadenas de texto
-
-const { warn } = require("vue");
+//const { warn } = require("vue");
 
 const alumno = "Erick Matias Granillo"
 let producto1 = 'Monitor 20 pulgadas'
@@ -49,7 +48,8 @@ console.warn("--- Tipo de dato - Null (Nulo)")
 
 
 //clase de profe mark
-
+const bg = "linear-gradient(11deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 33%, rgba(0,212,225.1) 86%)";
+const style_console = `background: ${bg}; color: white; border-radius: 6px; padding 4px;  font-size:1.0rem; font-weigh: bold`
 const producto = {
     nombre: "Tablet 9",
     marca: "mac",
@@ -65,3 +65,62 @@ console.table(producto)
 //Acceder a las propiedades de un objeto
 console.warn("Leyendo las propiedades dde un objeto y sus tipos de datos")
 console.log()
+
+console.log("en") 
+
+
+
+
+//backlog
+//Ahora leemos el objeto completo
+const producto2hol2 = {
+    clave: 555,
+    imagen: `/ASSETS/windows-tablet`,
+    marca: "windows",
+    modelo: "wilde3/",
+    nombre: "Tablet 12",
+    precio: "3692",
+    disponibilidad: true,
+    stock: 5,
+    SKU: Symbol("55366D546442"),
+    colores: ["Blanco","Rojo","negro","amarrillo", "rosa", "azul"],
+    barcode: 923213,
+    categorias: ["Deportes","Lentes","Hombre","Accesorios"],
+}
+let comprador =
+{
+    clave: 3216,
+    nombre: "matias",
+    apellidos:"Granillo Mejia",
+    tipo: "frecuente",
+    correo: "egranillomejia@gmail.com",
+    PaisOrigen: "Mexico",
+    SaldoActual: 14000.00,
+}
+let pedido =
+{
+    Producto_clave: 3216,
+    comprador_clave: 3456,
+    cantidad: 3,
+    Estatus: "carrito de comprar",
+    TipoPago: "targeta de credito",
+}
+
+//en base a los 3 objetos necesitamos calcular el costo de la compra y si le alcanza con sus saldo a favor 
+
+let {precio: producto_precio2 } = producto2hol2;
+let {cantidad: pedido_cantidad } = pedido;
+let {SaldoActual: Cliente_SaldoActual} = comprador;
+let costo_compra = producto2hol2_precio2 * pedido_cantidad;
+
+
+
+
+console.table(productohol2)
+console.log("Accediendo a propiedades especificas del producto")
+console.log(`Nombre Completo del producto: ${producto2hol2.nombre} ${producto.marca} ${producto.modelo}`)
+console.log(`precio: ${producto2hol2.precio}`)
+if(producto.disponibilidad == 0)
+    console.log(`el cliente ha agreado a su carrito de compras ${pedido_cantidad} undidades, con un costo total de : $${costo_compra}`);
+if(costo_compra< Cliente_SaldoActual)
+    console.log("El cliente tiene saldo suficiente")
